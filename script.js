dec12200aff8016e2127e371f668a6bce6e65ba1
rubenverkuylen <info@rubenverkuylen.nl>
2023-06-25 19:29:37 +0700

    random characters and links update

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function randomColor() {
+  [].forEach.call(document.querySelectorAll(".color-text"), function (el) {
+    el.style.color = `var(--color-text-${Math.floor(Math.random() * 10) + 1})`;
+  });
+}
+
+/*
+function randomColor() {
+  [].forEach.call(document.querySelectorAll(".ct1"), function (el) {
+    el.style.color = `#${Math.floor(Math.random() * (1 << 24)).toString(16)}`;
+  });
+}
+*/
+
+randomColor();

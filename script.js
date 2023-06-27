1774256357adfb71e36f02cf55fdfcc7d1ebba04
rubenverkuylen <info@rubenverkuylen.nl>
2023-06-27 18:27:13 +0700

    mobile update

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 "use strict";
 
+// EMAIL ENCODER
+var encEmail = "aW5mb0B3eXNpd3lnY2luZW1hLm5ldA==";
+const form = document.getElementById("contact");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+
 function randomColor() {
   [].forEach.call(document.querySelectorAll(".color-text"), function (el) {
     el.style.color = `var(--color-text-${Math.floor(Math.random() * 10) + 1})`;
   });
 }
-
-/*
-function randomColor() {
-  [].forEach.call(document.querySelectorAll(".ct1"), function (el) {
-    el.style.color = `#${Math.floor(Math.random() * (1 << 24)).toString(16)}`;
-  });
-}
-*/
 
 randomColor();
